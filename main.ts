c9620477bf9710798550490e1cc84894c8775c23
tung2222 <71689384+tung2222@users.noreply.github.com>
2020-09-22 15:01:50 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+basic.showNumber(0)
+basic.pause(1000)
+basic.showNumber(1)
+basic.pause(1000)
+basic.showNumber(2)
+basic.pause(1000)
+basic.showNumber(3)
+basic.pause(1000)
+basic.showNumber(4)
 basic.forever(function () {
-    basic.showNumber(6)
+	
 })
